c84ecb153280be749ce2c152bc60e8eb36c64e65
WissemMlouka <wissem.mlouka1235@gmail.com>
2025-07-24 12:43:56 +0100

    tenant id

--- xdev-base/src/main/java/com/xdev/xdevbase/config/TenantFilter.java
+++ xdev-base/src/main/java/com/xdev/xdevbase/config/TenantFilter.java
@@ -1,33 +1,33 @@
-package com.xdev.xdevbase.config;
-
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
-
-import java.io.IOException;
-import java.util.UUID;
-
-@Component
-public class TenantFilter extends OncePerRequestFilter {
-    @Override
-    protected void doFilterInternal(HttpServletRequest request,
-                                    HttpServletResponse response,
-                                    FilterChain filterChain)
-            throws ServletException, IOException {
-
-        try {
-
-            if(request.getHeader("X-Tenant-ID") != null) {
-                UUID tenantId = UUID.fromString(request.getHeader("X-Tenant-ID"));
-                TenantContext.setCurrentTenant(tenantId);
-            }
-            filterChain.doFilter(request, response);
-        } finally {
-            TenantContext.clear();
-        }
-    }
-}
+//package com.xdev.xdevbase.config;
+//
+//import jakarta.servlet.FilterChain;
+//import jakarta.servlet.ServletException;
+//import jakarta.servlet.http.HttpServlet;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.filter.OncePerRequestFilter;
+//
+//import java.io.IOException;
+//import java.util.UUID;
+//
+//@Component
+//public class TenantFilter extends OncePerRequestFilter {
+//    @Override
+//    protected void doFilterInternal(HttpServletRequest request,
+//                                    HttpServletResponse response,
+//                                    FilterChain filterChain)
+//            throws ServletException, IOException {
+//
+//        try {
+//
+//            if(request.getHeader("X-Tenant-ID") != null) {
+//                UUID tenantId = UUID.fromString(request.getHeader("X-Tenant-ID"));
+//                TenantContext.setCurrentTenant(tenantId);
+//            }
+//            filterChain.doFilter(request, response);
+//        } finally {
+//            TenantContext.clear();
+//        }
+//    }
+//}
---
+++ xdev-security/src/main/java/com/xdev/xdevsecurity/config/TenantContext.java
@@ -0,0 +1,19 @@
+package com.xdev.xdevsecurity.config;
+
+import java.util.UUID;
+
+public class TenantContext {
+    private static final ThreadLocal<UUID> currentTenant = new ThreadLocal<>();
+
+    public static void setCurrentTenant(UUID tenantId) {
+        currentTenant.set(tenantId);
+    }
+
+    public static UUID getCurrentTenant() {
+        return currentTenant.get();
+    }
+
+    public static void clear() {
+        currentTenant.remove();
+    }
+}
