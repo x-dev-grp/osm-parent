c51cd999cf9b4c10c8c3f9fb4885cd67c0c15954
Smail <ismailmansouri571@gmail.com>
2025-10-20 23:48:01 +0100

    Revert "jasper reporting"
    This reverts commit 5e4ebdee6b713774ba346c054d467b84efb45ad5.

--- xdev-base/src/main/java/com/xdev/xdevbase/config/exceptions/TemplateNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.xdev.xdevbase.config.exceptions;
-
-public class TemplateNotFoundException extends RuntimeException {
-    public TemplateNotFoundException(String message) {
-        super(message);
-    }
-}
